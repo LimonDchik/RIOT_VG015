1736e6cd71247bbab9da5193fd4a219b39a89d0c
nastyaskysat <nastyaskysat11@gmail.com>
2026-08-22 23:11:39 +0300

    added clean pwm

--- cpu/k1921vg015/periph/k1921_sys_min.c
+++ cpu/k1921vg015/periph/k1921_sys_min.c
@@ -5,6 +5,8 @@
 
 #include <stdint.h>
 
+#include "K1921VG015.h"
+
 #ifndef HSICLK_VAL
 #define HSICLK_VAL (1000000U)
 #endif
@@ -19,10 +21,26 @@ void SystemCoreClockUpdate(void)
     SystemPll0Clock = 0;
     SystemPll1Clock = 0;
     USBClock = 0;
-    SystemCoreClock = HSICLK_VAL;
+    switch ((RCU->CLKSTAT & RCU_CLKSTAT_SRC_Msk) >>
+            RCU_CLKSTAT_SRC_Pos) {
+        case RCU_CLKSTAT_SRC_HSICLK:
+            SystemCoreClock = HSICLK_VAL;
+            break;
+        case RCU_CLKSTAT_SRC_LSICLK:
+            SystemCoreClock = 32768U;
+            break;
+        default:
+            SystemCoreClock = 0;
+            break;
+    }
 }
 
 void SystemInit(void)
 {
+    RCU->SYSCLKCFG = RCU_SYSCLKCFG_SRC_HSICLK << RCU_SYSCLKCFG_SRC_Pos;
+    while (((RCU->CLKSTAT & RCU_CLKSTAT_SRC_Msk) >>
+            RCU_CLKSTAT_SRC_Pos) != RCU_CLKSTAT_SRC_HSICLK) {
+    }
+
     SystemCoreClockUpdate();
 }
